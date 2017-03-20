f9eb4667ff678163d581b4a4c2ee2595c493b598
jeffrey4518 <c.h.j.tong-15@student.lboro.ac.uk>
2017-03-20 16:00:21 +0000

    edit booking code change

--- app/src/main/java/com/taxiapp/group28/taxiapp/EditBookingActivity.java
+++ app/src/main/java/com/taxiapp/group28/taxiapp/EditBookingActivity.java
@@ -8,6 +8,6 @@ public class EditBookingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_edit_booking);
     }
 }
