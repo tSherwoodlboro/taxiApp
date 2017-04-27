8fedbf633a2fee83a4c65db5ed08cc506e66d043
Tom <t.sherwood-15@student.lboro.ac.uk>
2017-04-27 15:16:50 +0100

    Back button issues fixed (I think).

--- app/src/main/java/com/taxiapp/group28/taxiapp/MainMenuActivity.java
+++ app/src/main/java/com/taxiapp/group28/taxiapp/MainMenuActivity.java
@@ -31,7 +31,9 @@ public class MainMenuActivity extends AppCompatActivity {
     public static final int SETTINGS_FRAGMENT_POSITION = 4;
     public static final int ADD_ROUTES_FRAGMENT_KEY = 5;
     public static final int UPDATE_BOOKINGS_FRAGMENT_KEY = 6;
+    public static boolean liveBooking= false;
     private Fragment currentFragment = null;
+    private FragmentManager fragmentManager=null;
     private String key;
     @Override
     public void onCreate(Bundle saveInstanceState){
@@ -43,7 +45,9 @@ public class MainMenuActivity extends AppCompatActivity {
 
         drawerListView.setAdapter(new ArrayAdapter<>(this,R.layout.drawer_list_item,fragmentTitles));
         drawerListView.setOnItemClickListener(new DrawerItemClickListener());
-
+        if(fragmentManager == null){
+            fragmentManager = this.getFragmentManager();
+        }
         if(saveInstanceState == null) {
             key = Integer.toString(VIEW_BOOKINGS_FRAGMENT_POSITION);
             loadFragment(new ViewBookingsFragment(), VIEW_BOOKINGS_FRAGMENT_POSITION,false);
@@ -69,7 +73,7 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
+        Fragment fragment = fragmentManager.findFragmentById(R.id.content_frame);
         if(fragment!=null) {
             savedInstanceState.putString("fragmentTag", fragment.getTag());
         }
@@ -79,7 +83,7 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         if(savedInstanceState !=null && savedInstanceState.getString("fragmentTag") != null) {
             int position = Integer.valueOf(savedInstanceState.getString("fragmentTag"));
-            loadFragment(getFragmentManager().findFragmentByTag(savedInstanceState.getString("fragmentTag")), position,false);
+            loadFragment(fragmentManager.findFragmentByTag(savedInstanceState.getString("fragmentTag")), position,false);
         }
     }
     private class DrawerItemClickListener implements ListView.OnItemClickListener{
@@ -115,23 +119,27 @@ public class MainMenuActivity extends AppCompatActivity {
     }
     @Override
     public void  onBackPressed(){
-        super.onBackPressed();
-        if(currentFragment == null){
-            return;
-        }
-        int position =0;
-        try {
-            position = Integer.valueOf(currentFragment.getTag());
-        }catch(Exception e){
-            position = VIEW_BOOKINGS_FRAGMENT_POSITION; // set default
-        }finally {
-            try {
-                drawerListView.setItemChecked(position, true);
-                setTitle(fragmentTitles[position]);
-            }catch (Exception e){
-
+        if(fragmentManager.getBackStackEntryCount() <=1){
+            // if the stack is equal to 1 close program (Nothing to go back to)
+            this.finish();
+        }else {
+            printFragmentStack("0Before");
+            fragmentManager.popBackStackImmediate(); // pop the current element
+            printFragmentStack("0After");
+            Fragment fragment = fragmentManager.findFragmentByTag(fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount()-1).getName()); // get the latest added entry
+            if(fragment !=null) {
+                loadFragment(fragment, Integer.valueOf(fragment.getTag()), false); // load the previous fragment
+            }else{
+                Log.d("FRAGMENT_STACK", "FRAGMENT state: null");
             }
         }
+    }
+    private void printFragmentStack(String message){
+        String result ="";
+        for(int entry = 0; entry < fragmentManager.getBackStackEntryCount(); entry++){
+            result+=  fragmentManager.findFragmentByTag(fragmentManager.getBackStackEntryAt(entry).getName()).getTag()+ " ";
+        }
+        Log.i("FRAGMENT_STACK", message+": " + result);
     }
     public void setDrawerPosition(int pos){
         drawerListView.setItemChecked(pos, true);
@@ -139,29 +147,30 @@ public class MainMenuActivity extends AppCompatActivity {
         drawerLayout.closeDrawer(drawerListView);
     }
     public void loadFragment(Fragment fragment, int position, boolean loadNewFragment){
-        currentFragment = fragment;
-        FragmentManager fragmentManager = this.getFragmentManager();
-        key = String.valueOf(position);
+        currentFragment = fragment; // set current fragment
+        key = String.valueOf(position); // set the key/tag
+        Log.d("FRAGMENT_STACK","Key: "+key);
+        printFragmentStack("1Before");
         if(!loadNewFragment && fragmentManager.findFragmentByTag(key) != null){
-            // load fragment on stack
-            fragment = fragmentManager.findFragmentByTag(key);
+            // if the fragment isn't new  or doesn't need to be reloaded. fetch fragment
+            currentFragment = fragmentManager.findFragmentByTag(key);
         }
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        if(loadNewFragment || fragmentManager.findFragmentByTag(key) == null){
-            if(loadNewFragment && fragmentManager.findFragmentByTag(key) != null){
-                fragmentTransaction.remove(fragmentManager.findFragmentByTag(key));
-            }
-            fragmentTransaction.addToBackStack(key);
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); // begin transaction
+        if(fragmentManager.getBackStackEntryCount() == 0 || fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount()-1).getName() !=key){
+            fragmentTransaction.addToBackStack(key); // add to stack if stack empty or not previously in the stack
+            Log.d("FRAGMENT_STACK","Added to stack");
         }
-        fragmentTransaction.replace(R.id.content_frame, fragment,key);
-        fragmentTransaction.commit();
+        // replace the container in the activity with the current fragment
+        fragmentTransaction.replace(R.id.content_frame, currentFragment,key);
+        fragmentTransaction.commit(); // commit the transaction
+        printFragmentStack("1After:");
         // Highlight the selected item, update the title, and close the drawer
         drawerListView.setItemChecked(position, true);
         setTitle(fragmentTitles[position]);
         drawerLayout.closeDrawer(drawerListView);
     }
     public static ArrayAdapter<String> getNoResultAdapter(Context context){
+        // add a no results view to listView
         List<String> list = new ArrayList<>();
         list.add(context.getString(R.string.items_empty_text));
         ArrayAdapter<String> adapter = new ArrayAdapter<>(context,R.layout.items_empty_row,R.id.item_empty_text_view,list);
